6d72c505343d1b2d92a5ceb821f5e8dc41ccb7c4
Kyle Urban <Kyle Urban>
2017-02-09 17:40:36 -0500

    second attempt

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,6 +9,32 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', ['$scope', '$firebaseArray', function($scope, $firebaseArray) {
+  var database = firebase.database();
+  var refDatabase = firebase.database().ref();
+  var users = $firebaseArray(refDatabase.child('users'))
 
+  $scope.input = "Type Here"
+  $scope.change = function(){
+    console.log("Change")
+    
+    users.$add({
+      username: $scope.input
+    }).then(function(){
+      update()
+    })
+  }
+
+  var update = function(){
+    console.log("update")
+    $scope.usernames = [];
+    users.$loaded().then(function(user){
+      angular.forEach(user, function(x){
+        var temp = {name: x.username}
+        $scope.usernames.push(temp)
+      })
+      console.log($scope.usernames)
+    })
+  }
+  update()
 }]);
